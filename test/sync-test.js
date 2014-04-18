b97b4a4f2bb5a9dbe5311e3758268a113d6b1efe
Justin <jus.russell@gmail.com>
2014-04-18 07:58:09 -0400

    test(sync): Add test for reading from package.json

---
+++ test/sync-test.js
@@ -0,0 +1,19 @@
+/*global describe, it */
+
+'use strict';
+
+var expect =  require('chai').expect
+  , j = require('path').join
+  , getRootSync = require('../lib/svn-project-root').sync;
+
+describe('svn-project-root#sync', function() {
+
+  it('should be able to get url from package.json', function() {
+    var projFldr = j(__dirname, 'fixtures/svn-project-w-package-json/lib')
+      , root = getRootSync(projFldr);
+    expect(root).to.equal('https://svnhub.com/foo/bar');
+  });
+
+});
+
+
